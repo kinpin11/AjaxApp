a74a1828ae25a65fdc70da8a1377497fd35ed7c9
yoppy-luppy <ysk11knst@gmail.com>
2020-07-31 14:37:23 +0900

    revert

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
 
   def create
     post = Post.create(content: params[:content], checked: false)
-    render json:{ post: post } 
+    redirect json:{ post: post}
   end
 
   def checked
